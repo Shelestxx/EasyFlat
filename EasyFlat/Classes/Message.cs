b022effe2605b88642048b1fb44e4923abe6512d
Shelestxx <olaru30072007@gmail.com>
2025-03-01 22:53:23 +0200

    change message class

--- EasyFlat/Classes/Message.cs
+++ EasyFlat/Classes/Message.cs
@@ -6,8 +6,22 @@ using System.Threading.Tasks;
 
 namespace EasyFlat.Classes
 {
-    internal class Message
-    {
+        public class Message
+        {
+            public int ID { get; }
+            public int SenderID { get; }
+            public int ReceiverID { get; }
+            public string Text { get; }
+            public DateTime SentDate { get; }
 
+            public Message(int id, int senderID, int receiverID, string text, DateTime sentDate)
+            {
+                ID = id;
+                SenderID = senderID;
+                ReceiverID = receiverID;
+                Text = text;
+                SentDate = sentDate;
+            }
+
+        }
     }
-}
