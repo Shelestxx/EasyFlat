e660add06636e25f16e7271d0e9bf034315781a8
Олег Шлапак <olegshlapak2007@gmail.com>
2025-03-11 14:49:32 +0200

    Popytka

---
+++ EasyFlat/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyFlat.Repository;
+
+namespace EasyFlat.Repository
+{ 
+
+    public interface IRepository<T>
+    {
+        void Add(T entity);
+        void Remove(T entity);
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> GetSorted();
+    }
+}
