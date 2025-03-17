4d0d29eb04542fe6e0af5aae4fb4415767d25005
Олег Шлапак <olegshlapak2007@gmail.com>
2025-03-17 23:01:35 +0200

    beforeCosetic

--- EasyFlat/AddApartmentForm.Designer.cs
+++ EasyFlat/AddApartmentForm.Designer.cs
@@ -35,72 +35,138 @@
             txtRoomCount = new TextBox();
             txtArea = new TextBox();
             btnOK = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
             SuspendLayout();
             // 
             // txtTitle
             // 
-            txtTitle.Location = new Point(114, 32);
+            txtTitle.Location = new Point(155, 40);
             txtTitle.Margin = new Padding(2);
             txtTitle.Name = "txtTitle";
-            txtTitle.Size = new Size(121, 27);
+            txtTitle.Size = new Size(150, 31);
             txtTitle.TabIndex = 0;
             // 
             // txtDescription
             // 
-            txtDescription.Location = new Point(114, 91);
+            txtDescription.Location = new Point(155, 114);
             txtDescription.Margin = new Padding(2);
             txtDescription.Name = "txtDescription";
-            txtDescription.Size = new Size(121, 27);
+            txtDescription.Size = new Size(150, 31);
             txtDescription.TabIndex = 1;
             // 
             // txtLocation
             // 
-            txtLocation.Location = new Point(114, 155);
+            txtLocation.Location = new Point(155, 197);
             txtLocation.Margin = new Padding(2);
             txtLocation.Name = "txtLocation";
-            txtLocation.Size = new Size(121, 27);
+            txtLocation.Size = new Size(150, 31);
             txtLocation.TabIndex = 2;
             // 
             // txtRentPrice
             // 
-            txtRentPrice.Location = new Point(114, 213);
+            txtRentPrice.Location = new Point(155, 269);
             txtRentPrice.Margin = new Padding(2);
             txtRentPrice.Name = "txtRentPrice";
-            txtRentPrice.Size = new Size(121, 27);
+            txtRentPrice.Size = new Size(150, 31);
             txtRentPrice.TabIndex = 3;
             // 
             // txtRoomCount
             // 
-            txtRoomCount.Location = new Point(114, 269);
+            txtRoomCount.Location = new Point(155, 336);
             txtRoomCount.Margin = new Padding(2);
             txtRoomCount.Name = "txtRoomCount";
-            txtRoomCount.Size = new Size(121, 27);
+            txtRoomCount.Size = new Size(150, 31);
             txtRoomCount.TabIndex = 4;
             // 
             // txtArea
             // 
-            txtArea.Location = new Point(114, 325);
+            txtArea.Location = new Point(155, 406);
             txtArea.Margin = new Padding(2);
             txtArea.Name = "txtArea";
-            txtArea.Size = new Size(121, 27);
+            txtArea.Size = new Size(150, 31);
             txtArea.TabIndex = 5;
             // 
             // btnOK
             // 
-            btnOK.Location = new Point(426, 195);
+            btnOK.Location = new Point(497, 241);
             btnOK.Margin = new Padding(2);
             btnOK.Name = "btnOK";
-            btnOK.Size = new Size(90, 27);
+            btnOK.Size = new Size(231, 34);
             btnOK.TabIndex = 7;
-            btnOK.Text = "button1";
+            btnOK.Text = "Створити оголошення";
             btnOK.UseVisualStyleBackColor = true;
             btnOK.Click += btnOK_Click;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(89, 43);
+            label1.Name = "label1";
+            label1.Size = new Size(61, 25);
+            label1.TabIndex = 8;
+            label1.Text = "Назва";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(96, 117);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 25);
+            label2.TabIndex = 9;
+            label2.Text = "Опис";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(79, 200);
+            label3.Name = "label3";
+            label3.Size = new Size(71, 25);
+            label3.TabIndex = 10;
+            label3.Text = "Адреса";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(102, 275);
+            label4.Name = "label4";
+            label4.Size = new Size(48, 25);
+            label4.TabIndex = 11;
+            label4.Text = "Ціна";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 342);
+            label5.Name = "label5";
+            label5.Size = new Size(138, 25);
+            label5.TabIndex = 12;
+            label5.Text = "Кількість кімнат";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(81, 409);
+            label6.Name = "label6";
+            label6.Size = new Size(69, 25);
+            label6.TabIndex = 13;
+            label6.Text = "Площа";
+            // 
             // AddApartmentForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(640, 458);
+            ClientSize = new Size(800, 572);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(btnOK);
             Controls.Add(txtArea);
             Controls.Add(txtRoomCount);
@@ -124,5 +190,11 @@
         private TextBox txtRoomCount;
         private TextBox txtArea;
         private Button btnOK;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
     }
 }
