944ee39628ab86a04bdda964921e2d0ae4b18c8a
Shelestxx <olaru30072007@gmail.com>
2025-05-28 17:49:49 +0300

    unittest

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,42 @@
+using System;
+using Xunit;
+using EasyFlat;
+using EasyFlat.Classes;
+namespace UnitTestProject1
+{
+    public class UserTests
+    {
+        [Fact]
+        public void UserInitialization_ShouldSetPropertiesCorrectly()
+        {
+            var user = new User(1, "Oleh", "oleh@example.com", "password", "1234567890", UserType.Regular);
+
+            Assert.Equal(1, user.ID);
+            Assert.Equal("Oleh", user.Name);
+            Assert.Equal("oleh@example.com", user.Email);
+            Assert.Equal("password", user.Pass);
+            Assert.Equal("1234567890", user.PhoneNumber);
+            Assert.Equal(UserType.Regular, user.Type);
+        }
+
+        [Fact]
+        public void PropertyUser_RentProperty_ShouldAddAddress()
+        {
+            var user = new PropertyUser(2, "Oleh", "oleh@example.com", "pass", "0987654321");
+
+            user.RentProperty("вул. Шевченка, 10");
+
+            Assert.Contains("вул. Шевченка, 10", user.RentalAddresses);
+        }
+
+        [Fact]
+        public void PropertyUser_ListProperty_ShouldIncrementPropertiesOwned()
+        {
+            var user = new PropertyUser(3, "Oleh", "oleh@example.com", "pass", "1122334455");
+
+            user.ListProperty(2);
+
+            Assert.Equal(2, user.PropertiesOwned);
+        }
+    }
+}
